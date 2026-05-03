4d976e6a00b1478129574599f3d316ccbf235d45
Sarthak Midhotiya <sarthakmidhotiya@gmail.com>
2026-05-03 07:29:49 +0530

    Add files via upload

---
+++ arrayindex.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+        
+        System.out.println(arr.length);
+        
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
